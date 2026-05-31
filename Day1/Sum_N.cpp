19190bf38da8846fbc2d4faaaa52592bb8775c84
ishivam111 <ishivam1610@gmail.com>
2026-06-01 01:45:11 +0530

    Day 1: added file sum of n numbers

---
+++ Day1/Sum_N.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, sum = 0;
+    cout << "enter a number: ";
+    cin >> n;
+    sum = (n / 2) * (2 + (n - 1));
+    cout << "value of sum is= " << sum;
+    return 0;
+}
